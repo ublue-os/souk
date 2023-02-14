a0051e431d162a58eab580375cdb6e8029aad1a6
Felix Häcker <haeckerfelix@gnome.org>
2023-02-14 23:29:29 +0100

    installation_row: Port to properties macro / new pattern

--- src/main/ui/installation/installation_row.rs
+++ src/main/ui/installation/installation_row.rs
@@ -18,9 +18,8 @@ use std::cell::Cell;
 
 use adw::prelude::*;
 use adw::subclass::prelude::*;
-use glib::{subclass, ParamFlags, ParamSpec, ParamSpecBoolean, ParamSpecObject};
+use glib::{ParamSpec, Properties};
 use gtk::{glib, CompositeTemplate};
-use once_cell::sync::Lazy;
 use once_cell::unsync::OnceCell;
 
 use crate::main::flatpak::installation::SkInstallation;
@@ -28,14 +27,17 @@ use crate::main::flatpak::installation::SkInstallation;
 mod imp {
     use super::*;
 
-    #[derive(Default, Debug, CompositeTemplate)]
+    #[derive(Default, Debug, CompositeTemplate, Properties)]
+    #[properties(wrapper_type = super::SkInstallationRow)]
     #[template(resource = "/de/haeckerfelix/Souk/gtk/installation_row.ui")]
     pub struct SkInstallationRow {
+        #[property(get, set, construct_only)]
+        pub installation: OnceCell<SkInstallation>,
+        #[property(get, set)]
+        pub selected: Cell<bool>,
+
         #[template_child]
         pub checkmark: TemplateChild<gtk::Image>,
-
-        pub installation: OnceCell<SkInstallation>,
-        pub selected: Cell<bool>,
     }
 
     #[glib::object_subclass]
@@ -48,47 +50,35 @@ mod imp {
             Self::bind_template(klass);
         }
 
-        fn instance_init(obj: &subclass::InitializingObject<Self>) {
+        fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
             obj.init_template();
         }
     }
 
     impl ObjectImpl for SkInstallationRow {
         fn properties() -> &'static [ParamSpec] {
-            static PROPERTIES: Lazy<Vec<ParamSpec>> = Lazy::new(|| {
-                vec![
-                    ParamSpecObject::new(
-                        "installation",
-                        "",
-                        "",
-                        SkInstallation::static_type(),
-                        ParamFlags::READWRITE | ParamFlags::CONSTRUCT_ONLY,
-                    ),
-                    ParamSpecBoolean::new("selected", "", "Selected", false, ParamFlags::READWRITE),
-                ]
-            });
-            PROPERTIES.as_ref()
+            Self::derived_properties()
         }
 
-        fn property(&self, _id: usize, pspec: &ParamSpec) -> glib::Value {
-            match pspec.name() {
-                "installation" => self.obj().installation().to_value(),
-                "selected" => self.obj().selected().to_value(),
-                _ => unimplemented!(),
-            }
+        fn property(&self, id: usize, pspec: &ParamSpec) -> glib::Value {
+            Self::derived_property(self, id, pspec)
         }
 
-        fn set_property(&self, _id: usize, value: &glib::Value, pspec: &ParamSpec) {
-            match pspec.name() {
-                "installation" => self.installation.set(value.get().unwrap()).unwrap(),
-                "selected" => self.obj().set_selected(value.get().unwrap()),
-                _ => unimplemented!(),
-            }
+        fn set_property(&self, id: usize, value: &glib::Value, pspec: &ParamSpec) {
+            Self::derived_set_property(self, id, value, pspec)
         }
 
         fn constructed(&self) {
             self.parent_constructed();
-            self.obj().setup_widgets();
+            let installation = self.obj().installation();
+
+            self.obj().set_icon_name(Some(&installation.icon_name()));
+            self.obj().set_title(&installation.title());
+            self.obj().set_subtitle(&installation.description());
+
+            self.obj()
+                .bind_property("selected", &self.checkmark.get(), "visible")
+                .build();
         }
     }
 
@@ -113,30 +103,5 @@ impl SkInstallationRow {
         glib::Object::builder()
             .property("installation", installation)
             .build()
-    }
-
-    fn setup_widgets(&self) {
-        let imp = self.imp();
-        let installation = self.installation();
-
-        self.set_icon_name(Some(&installation.icon_name()));
-        self.set_title(&installation.title());
-        self.set_subtitle(&installation.description());
-
-        self.bind_property("selected", &imp.checkmark.get(), "visible")
-            .build();
-    }
-
-    pub fn installation(&self) -> SkInstallation {
-        self.imp().installation.get().unwrap().clone()
-    }
-
-    pub fn set_selected(&self, selected: bool) {
-        self.imp().selected.set(selected);
-        self.notify("selected");
-    }
-
-    pub fn selected(&self) -> bool {
-        self.imp().selected.get()
     }
 }
